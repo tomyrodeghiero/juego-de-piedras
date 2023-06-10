06aa40ca84499fd8f6d5fff2602fdeae49778d5d
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-06-10 19:21:09 -0300

    Fix compilation 4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { ignoreBuildErrors: true };
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 module.exports = nextConfig;
